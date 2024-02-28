f0b8866d2394daa979a34a85a55c0d8d6153005f
alrula@jviladoms.cat <albertonax04@gmail.com>
2024-02-28 13:01:49 +0100

    subida de archivos

---
+++ WebPersonal.js
@@ -0,0 +1,29 @@
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.querySelector('.menu2').style.display = document.querySelector('.menu2').style.display === 'block' ? 'none' : 'block';
+});
+
+function IrACyberpunk() {
+    window.open("https://madnaxss.github.io/CyberpunkForm.github.io/formulari2.html", "_blank");
+}
+
+function IrAJocNadal() {
+    window.open("https://madnaxss.github.io/JuegoNavidad.github.io/JocNadal.html", "_blank");
+}
+
+function IrAAkinator() {
+    window.open("https://madnaxss.github.io/Akinator.github.io/Akinator2.html", "_blank");
+}
+
+function IrATresEnRaya() {
+    window.open("https://madnaxss.github.io/TresEnRaya.github.io/Tres_En_Raya.html", "_blank");
+}
+
+function IrAInstagram()
+{
+    window.open("https://www.instagram.com/_.albertoruiz._/", "_blank");
+}
+
+function IrATwitter()
+{
+    window.open("https://x.com/madnuxiss?t=MSjsNPpflm0_Eofp6Vg1FA&s=09", "_blank");
+}
